f09590a9a759f4e2502a898be02c670b8c2413e7
Carl C <69673640+awsomesawce@users.noreply.github.com>
2021-10-04 11:05:03 -0400

    Update

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace dotnet_stuff
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Another line");
         }
     }
 }
--- UsefulFunctions.psm1
+++ UsefulFunctions.psm1
@@ -1,3 +1,31 @@
+<#
+.Description
+Invoke the global paket executable
+#>
 function paket {
 (gcm dotnet -erroraction ignore) ? (dotnet tool run paket -- "$args") : (write-error "Dotnet not installed")
 }
+
+<#
+.Description
+Go back one dir
+#>
+function cdBackOne {
+if (test-path ..) {Set-Location ..} else {write-warning "No parent dir"}
+}
+
+<#
+.Description
+Go back two dirs
+#>
+function cdBackTwo {
+param()
+(test-path ../..) ? (Set-Location ../..) : `
+(write-warning "parent of parent dir not found`n Moving to parent dir" && set-location ..)
+}
+
+# Set aliases for cd functions:
+Set-Alias -Name ".." -Value cdBackOne -Description "Goes back one directory"
+Set-Alias -Name "..." -Value cdBackTwo -Description "Goes back two directories"
+
+export-modulemember -Function "*" -Alias "..", "..."
